adc53ca7726adf8f6b6b0a8b231222616f55c561
Ryan Kish <ryan.douglas.kish@gmail.com>
2024-11-30 20:11:23 -0500

    feat: add kanagawa

---
+++ lua/custom/plugins/kanagawa.lua
@@ -0,0 +1,35 @@
+return {
+    'rebelot/kanagawa.nvim',
+    lazy = false, -- Load the theme immediately or on startup
+    priority = 1000, -- Ensure it's loaded before other plugins
+    config = function()
+      -- Default options:
+      require('kanagawa').setup {
+        compile = false, -- enable compiling the colorscheme
+        undercurl = true, -- enable undercurls
+        commentStyle = { italic = true },
+        functionStyle = {},
+        keywordStyle = { italic = true },
+        statementStyle = { bold = true },
+        typeStyle = {},
+        transparent = false, -- do not set background color
+        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        terminalColors = true, -- define vim.g.terminal_color_{0,17}
+        colors = { -- add/modify theme and palette colors
+          palette = {},
+          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+        },
+        overrides = function(colors) -- add/modify highlights
+          return {}
+        end,
+        theme = 'wave', -- Load "wave" theme when 'background' option is not set
+        background = { -- map the value of 'background' option to a theme
+          -- dark = 'wave', -- try "dragon" !
+          dark = 'wave',
+          light = 'lotus',
+        },
+      }
+      vim.cmd 'colorscheme kanagawa'
+    end,
+  }
+  -- vim: ts=2 sts=2 sw=2 et
