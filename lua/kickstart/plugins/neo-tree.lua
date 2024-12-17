05c8123e71c3439fd9ab1b1739dd929a5b165ec0
Ryan Kish <ryan.douglas.kish@gmail.com>
2024-12-17 11:54:58 -0500

    fix(neotree): show hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -13,13 +13,26 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+  config = function(_, opts)
+    -- Add configuration to show hidden files
+    opts.filesystem = opts.filesystem or {}
+    opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
+    opts.filesystem.filtered_items.hide_dotfiles = false
+    opts.filesystem.filtered_items.hide_gitignored = false
+    opts.filesystem.filtered_items.never_show = {
+      '__pycache__',
+      '.DS_Store',
+    }
+    opts.filesystem.window = opts.filesystem.window or {}
+    opts.filesystem.window.mappings = opts.filesystem.window.mappings or {}
+    opts.filesystem.window.mappings['\\'] = 'close_window'
+
+    opts.filesystem.window.mappings['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } }
+    opts.filesystem.window.mappings['<C-b>'] = { 'scroll_preview', config = { direction = 10 } }
+    opts.filesystem.window.mappings['<C-f>'] = { 'scroll_preview', config = { direction = -10 } }
+
+    require('neo-tree').setup(vim.tbl_deep_extend('force', opts, {
+      close_if_last_window = true,
+    }))
+  end,
 }
