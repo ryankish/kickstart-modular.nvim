574fe78f71e944de4a9e8085e263b720fe7fc26c
Ryan Kish <ryan.douglas.kish@gmail.com>
2024-12-23 00:06:12 -0500

    feat: add refactor

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -91,4 +91,39 @@ vim.keymap.set('n', '<leader>q', function()
   vim.cmd('silent !zathura ' .. pdf_path .. ' &')
 end, { silent = true })
 
+-- refactoring
+vim.keymap.set('x', '<leader>re', ':Refactor extract ', { desc = '[R]efactor [E]xtract' })
+vim.keymap.set('x', '<leader>rf', ':Refactor extract_to_file ', { desc = '[R]efactor extract to [F]ile' })
+
+vim.keymap.set('x', '<leader>rv', ':Refactor extract_var ', { desc = '[R]efactor extract [V]ariable' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>ri', ':Refactor inline_var', { desc = '[R]efactor [I]nline variable' })
+
+vim.keymap.set('n', '<leader>rI', ':Refactor inline_func', { desc = '[R]efactor [I]nline function' })
+
+vim.keymap.set('n', '<leader>rb', ':Refactor extract_block', { desc = '[R]efactor extract [B]lock' })
+vim.keymap.set('n', '<leader>rbf', ':Refactor extract_block_to_file', { desc = '[R]efactor extract [B]lock to [F]ile' })
+
+vim.keymap.set({ 'n', 'x' }, '<leader>rr', function()
+  require('telescope').extensions.refactoring.refactors()
+end)
+
+-- You can also use below = true here to to change the position of the printf
+-- statement (or set two remaps for either one). This remap must be made in normal mode.
+vim.keymap.set('n', '<leader>rp', function()
+  require('refactoring').debug.printf { below = false }
+end)
+
+-- Print var
+
+vim.keymap.set({ 'x', 'n' }, '<leader>rv', function()
+  require('refactoring').debug.print_var()
+end)
+-- Supports both visual and normal mode
+
+vim.keymap.set('n', '<leader>rc', function()
+  require('refactoring').debug.cleanup {}
+end)
+-- Supports only normal mode
+
 -- vim: ts=2 sts=2 sw=2 et
