6d00d0a66d5040e85b1f3ff4b78d478a4592e7fd
Ryan Kish <ryan.douglas.kish@gmail.com>
2025-01-02 21:20:10 -0500

    feat(python): add python

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -220,6 +220,8 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'clangd',
+        'pyright',
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
