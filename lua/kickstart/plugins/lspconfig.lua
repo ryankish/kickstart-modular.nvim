55c28ceab5af4a778aa6ba30418d004ea641fd2c
Ryan Kish <ryan.douglas.kish@gmail.com>
2026-01-13 20:03:28 -0500

    Disable mason auto-install, use nix LSPs

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -206,40 +206,27 @@ return {
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --  To check the current status of installed tools and/or manually install
-      --  other tools, you can run
-      --    :Mason
-      --
-      --  You can press `g?` for help in this menu.
+      -- Mason setup (LSPs are installed via nix, so ensure_installed is empty)
       require('mason').setup()
-
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'clangd',
-        'codelldb',
-        'ts_ls', -- typescript_language_server
-        'tailwindcss',
-        'pyright',
-        'ruff',
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+      require('mason-tool-installer').setup { ensure_installed = {} }
 
       require('mason-lspconfig').setup {
+        automatic_installation = false,
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
         },
       }
+
+      -- Set up additional LSPs installed via nix
+      local lspconfig = require('lspconfig')
+      lspconfig.clangd.setup { capabilities = capabilities }
+      lspconfig.pyright.setup { capabilities = capabilities }
+      lspconfig.ruff.setup { capabilities = capabilities }
+      lspconfig.tailwindcss.setup { capabilities = capabilities }
     end,
   },
 }
